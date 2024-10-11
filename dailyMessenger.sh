effb525084eb5d9a0c016da79d058b947827df4b
William Miller <Wmilleiv@gmail.com>
2024-10-11 14:03:07 -0400

    added dailyMessenger

---
+++ dailyMessenger.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+dow=$(date +%u)
+case $dow in
+       	1)		
+		echo I hate Mondays
+	;;
+	2)
+		echo Is it Friday yet?
+	;;
+	3)
+		echo stop thinking about the day of the week
+	;;
+	4)
+		echo four-fifths done
+	;;
+	5)
+		echo Still a work day
+	;;
+	6)
+		echo Don\'t waste your one day of happiness
+	;;
+	7)
+		echo Tomorrow is a work day...
+	;;
+esac
