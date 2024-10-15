7156a0ce643d498f700dce5467e9408f67f99449
William Miller <Wmilleiv@gmail.com>
2024-10-14 21:15:09 -0400

    Code Generator function added

--- mastermind.sh
+++ mastermind.sh
@@ -6,9 +6,26 @@ echo Please try to find the correct partial anagram of \"security\" to crack the
 echo
 echo
 
-readonly CODEPARTS = ('s','e','c','u','r','i','t','y')
+#readonly CODEPARTS = ('s','e','c','u','r','i','t','y')
 
+#setCode(){
+	num=$RANDOM
+	firstChar=$(( $num / 4096 ))  
+	(( num -= ($firstChar * 4096) ))
+	secondChar=$(( $num / 512 ))
+	(( num -= ($secondChar * 512) ))
+	thirdChar=$(( $num / 64 ))
+	(( num -= ($thirdChar * 64) ))
+	fourthChar=$(( $num / 8 ))
+	(( num -= ($fourthChar * 8) ))
+	fifthChar=$num
+	echo $secondChar
+	echo $thirdChar
+	echo $fourthChar
+	echo $fifthChar
+#}
 
+#setCode
 
 for ((try=1;try<=8; try++))
 do	
