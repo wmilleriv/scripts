f9868b1740b7bb347bc23a605f1c61e5ff0c0f44
William Miller <Wmilleiv@gmail.com>
2024-10-17 20:24:35 -0400

    tic tac toe added

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,8 +1,84 @@
 #!/bin/bash
 
-echo "   | |    "
-echo " -------  "
-echo "   | |    "
-echo " -------  "
-echo "   | |    "
+echo "   A  B  C  "
+echo " 1   | |    "
+echo "   -------  "
+echo " 2   | |    "
+echo "   -------  "
+echo " 3   | |    "
+
+
+echo "Let's play Tic Tac Toe"
+echo "Rock Paper Scissors to go first"
+echo "type r for rock, p for paper or s for scissors"
+read playerChoice
+
+computerChoice=$(( $RANDOM %3 ))
+echo $computerChoice
+
+if [ $computerChoice = 0 ]
+then
+	echo "The computer chose rock"
+	if [ $playerChoice = p ]
+	then
+		echo "Congratulations you won"
+		#playerTurn #function to be added later
+	fi
+	if [ $playerChoice = s ]
+	then
+		echo "The computer will go first"
+		#computerTurn #function to be added
+	fi
+	if [ $playerChoice = r ]
+	then
+		echo "It's a tie. Try again"
+		#rockPaperScissors #put this in function and call again
+	fi	
+
+fi
+
+if [ $computerChoice = 1 ]
+then
+	echo "The computer chose paper"
+	if [ $playerChoice = s ]
+	then
+		echo "Congratulations you won"
+		#playerTurn #function to be added later
+	fi
+	if [ $playerChoice = r ]
+	then
+		echo "The computer will go first"
+		#computerTurn #function to be added
+	fi
+	if [ $playerChoice = p ]
+	then
+		echo "It's a tie. Try again"
+		#rockPaperScissors #put this in function and call again
+	fi	
+
+fi
+
+if [ $computerChoice = 2 ]
+then
+	echo "The computer chose paper"
+	if [ $playerChoice = r ]
+	then
+		echo "Congratulations you won"
+		#playerTurn #function to be added later
+	fi
+	if [ $playerChoice = p ]
+	then
+		echo "The computer will go first"
+		#computerTurn #function to be added
+	fi
+	if [ $playerChoice = s ]
+	then
+		echo "It's a tie. Try again"
+		#rockPaperScissors #put this in function and call again
+	fi	
+
+fi
+
+
+
 
